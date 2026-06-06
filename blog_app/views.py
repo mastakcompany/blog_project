aea4cdc32d62353d4e47ef56a8c492baeb37fc4f
mastak <companymastak@gmail.com>
2026-06-06 15:52:32 +0700

    lesson-5 done

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,40 +1,37 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 from blog_app.models import Post, Category
 
 
 def index(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    posts = Post.objects.filter(published=True)[:5]
+
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/index.html', context)
 
 
 def posts_list(request):
     posts = Post.objects.filter(published=True)
-
     content = '<h1>Опубликованные статьи</h1><br><br>'
-
     content += '<ul>'
 
     for post in posts:
         content += f'<li><a href="/posts/{post.slug}/">{post.title}</a> ({post.created_at})</li><br>'
 
     content += '</ul>'
-
     return HttpResponse(content)
 
 
 def post_detail(request, post_slug):
     post = get_object_or_404(Post, slug=post_slug)
 
-    content = f'''
-    <h1>{post.title}</h1>
-    <p>Автор: {post.autor}</p>
-    <div>{post.content}</div>
-    <hr>
-    <a href="/posts/">Назад к статьям</a>
-    '''
-
-    return HttpResponse(content)
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context)
 
 
 def category_list(request):
@@ -56,14 +53,12 @@ def category_detail(request, category_id):
     posts = Post.objects.filter(category=category, published=True)
 
     content = f'<h1>{category.title}</h1><br><br>'
-
     content += '<ul>'
 
     for post in posts:
         content += f'<li><a href="/posts/{post.slug}/">{post.title}</a> ({post.created_at})</li><br>'
 
     content += '</ul>'
-
     content += '<br><hr><br><a href="/categories/">Назад к категориям</a>'
 
     return HttpResponse(content)
