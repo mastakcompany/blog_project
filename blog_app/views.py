03fee2a1e29cd5cf4cb9a5e7418aadda5211b8cb
mastak <companymastak@gmail.com>
2026-06-14 12:58:51 +1000

    feat: migrated posts list to templates and configured local static bootstrap

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from blog_app.models import Post, Category
@@ -15,14 +14,10 @@ def index(request):
 
 def posts_list(request):
     posts = Post.objects.filter(published=True)
-    content = '<h1>Опубликованные статьи</h1><br><br>'
-    content += '<ul>'
-
-    for post in posts:
-        content += f'<li><a href="/posts/{post.slug}/">{post.title}</a> ({post.created_at})</li><br>'
-
-    content += '</ul>'
-    return HttpResponse(content)
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/posts_list.html', context)
 
 
 def post_detail(request, post_slug):
@@ -31,34 +26,25 @@ def post_detail(request, post_slug):
     context = {
         'post': post,
     }
+    post.increase_views_count()
     return render(request, 'blog/post_detail.html', context)
 
 
 def category_list(request):
     categories = Category.objects.all()
 
-    content = '<h1>Список всех категорий</h1><br><br>'
-    content += '<ul>'
-
-    for category in categories:
-        content += f'<li><a href="/categories/{category.id}/">{category.title}</a></li><br>'
-
-    content += '</ul>'
-
-    return HttpResponse(content)
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'blog/categories_list.html', context)
 
 
 def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     posts = Post.objects.filter(category=category, published=True)
 
-    content = f'<h1>{category.title}</h1><br><br>'
-    content += '<ul>'
-
-    for post in posts:
-        content += f'<li><a href="/posts/{post.slug}/">{post.title}</a> ({post.created_at})</li><br>'
-
-    content += '</ul>'
-    content += '<br><hr><br><a href="/categories/">Назад к категориям</a>'
-
-    return HttpResponse(content)
+    context = {
+        'category': category,
+        'posts': posts,
+    }
+    return render(request, 'blog/category_detail.html', context)
