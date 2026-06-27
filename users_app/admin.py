7e24da33dffd1470365022dbe80f0364efaa325b
mastak <companymastak@gmail.com>
2026-06-27 15:26:53 +1000

    feat: миксины и оптимизация CBV

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,1 +1,12 @@
-# Register your models here.
+from django.contrib import admin
+
+from users_app.views import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'bio',
+        'social_link',
+    )
